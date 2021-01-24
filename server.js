727ba887dd16c66717c4c1fd487dcede73773218
anchal gaur <anchal.gaur@decimal.co.in>
2021-01-24 23:55:31 +0530

    changes in server file

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(__dirname + '/dist/space-x'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/myapp/index.html'));});
+'/dist/space-x/index.html'));});
 app.listen(process.env.PORT || 8080);
