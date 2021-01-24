d4e1b5b419b299010053c6620f28d5f518f0095d
anchal gaur <anchal.gaur@decimal.co.in>
2021-01-24 18:45:40 +0530

    worked on filter integration

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,8 +7,9 @@ import { HomeService } from '../home/home.service';
 })
 export class HomeComponent implements OnInit {
   launchData: any;
-  lauchDataSuccess = [];
-  missionIds: any;
+  launchParam: any;
+  landParam: any;
+  lauchYearData = [];
   constructor(private homeService:HomeService) { }
 
   ngOnInit(): void {
@@ -18,9 +19,45 @@ export class HomeComponent implements OnInit {
   getSpaceXData(){
     this.homeService.getSpaceData().subscribe(res=>{
       this.launchData = res;
-      console.log('res', res);
+      this.lauchYearData = [];
+      for(let i in this.launchData){
+        if(this.lauchYearData.indexOf(this.launchData[i].launch_year) == -1){
+          this.lauchYearData.push(this.launchData[i].launch_year);
+          console.log("result..", this.lauchYearData);
+        }
+      }
     },err=>{
 
+    })
+  }
+
+  getLauchSuccessData(lauchParam){
+    this.launchParam = lauchParam
+    this.homeService.launchSuccessData(lauchParam).subscribe(res=>{
+      this.launchData = res;
+    })
+  }
+
+  getLauchAndLandData(lauchParam, landParam){
+    if(this.launchParam !== undefined){
+      lauchParam = this.launchParam
+    }
+    this.landParam = landParam
+    this.homeService.launchAndLandFilteredData(lauchParam, landParam).subscribe(res=>{
+      this.launchData = res;
+    })
+  }
+
+  getLauchLandAnddYearData(lauchParam, landParam, yearParam){
+    if(this.launchParam){
+      lauchParam = this.launchParam
+    }
+    if(this.landParam){
+      landParam = this.landParam
+    }
+    this.homeService.launchLandAndYearFilteredData(lauchParam, landParam, yearParam).subscribe(res=>{
+      this.launchData = res;
+      console.log("lauch success", res);
     })
   }
 
