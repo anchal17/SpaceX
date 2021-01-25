3b57291752c68fb449de042151e17dfdd77310c6
anchal gaur <anchal.gaur@decimal.co.in>
2021-01-25 19:51:53 +0530

    added favicon in server file

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+var favicon = require('serve-favicon')
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/space-x'));
+app.use(favicon(__dirname + '/dist/favicon.ico'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
 '/dist/space-x/index.html'));});
